c038b515a17df2dc743d3a10ce9e294c93f5a054
Ronielson Oliveira <ronielsonoliveira@live.com>
2019-11-12 20:47:14 -0300

    Projeto final web

---
+++ src/main/java/ufc/com/br/Trabalho2Application.java
@@ -0,0 +1,12 @@
+package ufc.com.br;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Trabalho2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Trabalho2Application.class, args);
+	}
+}
